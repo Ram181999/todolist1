5ea68f0706874e01cc8cd545d9f482336fbc8f7a
Your NameRam181999 <ramprasadgoudaineri@gmail.com>
2023-12-12 16:31:48 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,13 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "../src/App.css";
 
-import Task from './components/Task';
-function App() {
+const App = () => {
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const handleAdd = (e) => {
+    e.preventDefault();
+    setTodos([...todos, todo]);
+    setTodo("");
+  };
+
+  const handleDelete = (index) => {
+    setTodos(todos.filter((todo, i) => i !== index));
+  };
+
   return (
     <div>
-<Task></Task>
+      {/* Do not remove the main div */}
+      <h1>To-Do List</h1>
+      <form>
+        <input
+          type="text"
+          id="todo"
+          name="todo"
+          value={todo}
+          onChange={(e) => setTodo(e.target.value)}
+        />
+        <button onClick={handleAdd}>Add Todo</button>
+      </form>
+
+      <ul>
+        {todos.map((todo, index) => {
+          return (
+            <li key={index}>
+              <div className="todo-item">
+                <p>{todo}</p>
+                <button onClick={() => handleDelete(index)}>Delete</button>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
